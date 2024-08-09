735f477cdc24991add0156640320fa700237598d
Ming Li <ming.5.li@here.com>
2024-08-09 23:09:30 +0200

    Add ollama server option so that one can access it from a local network
    Signed-off-by: Ming Li <ming.5.li@here.com>

--- src/options.js
+++ src/options.js
@@ -1,8 +1,13 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
     const ollama_model = document.getElementById('ollama_model').value;
+    const ollama_server = document.getElementById('ollama_server').value;
+
     chrome.storage.sync.set(
-      { ollama_model: ollama_model },
+      {
+        ollama_model: ollama_model,
+        ollama_server: ollama_server
+      },
       () => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
@@ -18,10 +23,14 @@ const saveOptions = () => {
   // stored in chrome.storage.
   const restoreOptions = () => {
     chrome.storage.sync.get(
-      { ollama_model: "llama3.1:8b" },
+      { 
+        ollama_model: "llama3.1:8b",
+        ollama_server: "http://localhost:11434"
+      },
       (items) => {
         console.log(items)
         document.getElementById('ollama_model').value = items.ollama_model;
+        document.getElementById('ollama_server').value = items.ollama_server;
       }
     );
   };
