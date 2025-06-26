8004805ec17ce59fe8703bf7613da854ddb3847e
Ming Li <ming.5.li@here.com>
2025-06-26 09:39:58 +0200

    Add missing config.js containing the default guidelines
    Signed-off-by: Ming Li <ming.5.li@here.com>

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const DEFAULT_GUIDELINES = {
+  name: 'Default Guidelines',
+  content: `Your task is:
+    - Review the code changes and provide feedback.
+    - If you have a better version of the title and description of the merge request, please suggest in a dedicated section.
+    - If there are any bugs, highlight them.
+    - Provide details on missed use of best-practices.
+    - Does the code do what it says in the commit messages?
+    - Do not highlight minor issues and nitpicks.
+    - Use bullet points if you have multiple comments.`
+};
